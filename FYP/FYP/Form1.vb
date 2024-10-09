93c18965444ca61115373bc59998b1e0830c7d95
NgeZY <zheyunge@gmail.com>
2024-10-09 22:06:15 +0800

    .

--- FYP/FYP/Form1.vb
+++ FYP/FYP/Form1.vb
@@ -2,8 +2,4 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
     End Sub
-
-    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
-        Form2.Show()
-    End Sub
 End Class
