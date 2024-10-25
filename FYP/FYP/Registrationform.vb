32365affd32103c52ed0992c781bf89414a1c8fa
NgeZY <zheyunge@gmail.com>
2024-10-25 23:39:31 +0800

    User management

--- FYP/FYP/Registrationform.vb
+++ FYP/FYP/Registrationform.vb
@@ -1,7 +1,14 @@
-﻿Public Class Registrationform
+﻿Imports MySql.Data.MySqlClient
+Public Class Registrationform
 
     Private Sub ButtonRegister_Click(sender As Object, e As EventArgs) Handles ButtonRegister.Click
         Label3.Visible = True
+        Dim connectionString As String = "server=localhost; user=root; password=; database = school_management"
+        Using connection As New MySqlConnection(connectionString)
+            connection.Open()
+            Dim query As String = "INSERT INTO subject(StudentID, Subject_Code) VALUES (@id, @subject_code)"
+        End Using
+
     End Sub
 
     Private Sub ButtonDisplaySubject_Click(sender As Object, e As EventArgs) Handles ButtonDisplaySubject.Click
