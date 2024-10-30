14745c1d87f9b1fbe6852a473b38a601fb04a317
risaemylia <167045818+risaemylia@users.noreply.github.com>
2024-10-30 15:10:54 +0800

    edit coding for connection

--- FYP/FYP/TeacherInfo.Designer.vb
+++ FYP/FYP/TeacherInfo.Designer.vb
@@ -23,20 +23,20 @@ Partial Class TeacherInfo
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.txtBoxIcNumber = New System.Windows.Forms.TextBox()
+        Me.txtSubject = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtIcNumber = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblIcNumber = New System.Windows.Forms.Label()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblIc = New System.Windows.Forms.Label()
+        Me.lblSubject = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
         Me.Label7 = New System.Windows.Forms.Label()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -48,36 +48,36 @@ Partial Class TeacherInfo
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.RowHeadersWidth = 62
         Me.DataGridView1.RowTemplate.Height = 28
-        Me.DataGridView1.Size = New System.Drawing.Size(376, 36)
+        Me.DataGridView1.Size = New System.Drawing.Size(547, 254)
         Me.DataGridView1.TabIndex = 2
         '
-        'TextBox1
+        'txtBoxIcNumber
         '
-        Me.TextBox1.Location = New System.Drawing.Point(368, 155)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(427, 28)
-        Me.TextBox1.TabIndex = 3
+        Me.txtBoxIcNumber.Location = New System.Drawing.Point(368, 155)
+        Me.txtBoxIcNumber.Name = "txtBoxIcNumber"
+        Me.txtBoxIcNumber.Size = New System.Drawing.Size(427, 28)
+        Me.txtBoxIcNumber.TabIndex = 3
         '
-        'TextBox2
+        'txtSubject
         '
-        Me.TextBox2.Location = New System.Drawing.Point(854, 447)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(237, 28)
-        Me.TextBox2.TabIndex = 4
+        Me.txtSubject.Location = New System.Drawing.Point(854, 447)
+        Me.txtSubject.Name = "txtSubject"
+        Me.txtSubject.Size = New System.Drawing.Size(237, 28)
+        Me.txtSubject.TabIndex = 4
         '
-        'TextBox3
+        'txtName
         '
-        Me.TextBox3.Location = New System.Drawing.Point(854, 334)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(237, 28)
-        Me.TextBox3.TabIndex = 5
+        Me.txtName.Location = New System.Drawing.Point(854, 334)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(237, 28)
+        Me.txtName.TabIndex = 5
         '
-        'TextBox4
+        'txtIcNumber
         '
-        Me.TextBox4.Location = New System.Drawing.Point(854, 388)
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(237, 28)
-        Me.TextBox4.TabIndex = 6
+        Me.txtIcNumber.Location = New System.Drawing.Point(854, 388)
+        Me.txtIcNumber.Name = "txtIcNumber"
+        Me.txtIcNumber.Size = New System.Drawing.Size(237, 28)
+        Me.txtIcNumber.TabIndex = 6
         '
         'Label1
         '
@@ -89,51 +89,51 @@ Partial Class TeacherInfo
         Me.Label1.TabIndex = 7
         Me.Label1.Text = "Teacher Information"
         '
-        'Label2
+        'lblIcNumber
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(146, 154)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(185, 25)
-        Me.Label2.TabIndex = 8
-        Me.Label2.Text = "Name / IC Number :"
+        Me.lblIcNumber.AutoSize = True
+        Me.lblIcNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblIcNumber.Location = New System.Drawing.Point(146, 154)
+        Me.lblIcNumber.Name = "lblIcNumber"
+        Me.lblIcNumber.Size = New System.Drawing.Size(122, 25)
+        Me.lblIcNumber.TabIndex = 8
+        Me.lblIcNumber.Text = " IC Number :"
         '
-        'Button1
+        'btnSearch
         '
-        Me.Button1.Location = New System.Drawing.Point(888, 141)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(129, 41)
-        Me.Button1.TabIndex = 9
-        Me.Button1.Text = "Search"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnSearch.Location = New System.Drawing.Point(888, 141)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(129, 41)
+        Me.btnSearch.TabIndex = 9
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
         '
-        'Label3
+        'lblName
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(725, 334)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(101, 22)
-        Me.Label3.TabIndex = 10
-        Me.Label3.Text = "Full Name :"
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(725, 334)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(72, 22)
+        Me.lblName.TabIndex = 10
+        Me.lblName.Text = " Name :"
         '
-        'Label4
+        'lblIc
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(725, 388)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(105, 22)
-        Me.Label4.TabIndex = 11
-        Me.Label4.Text = "IC Number :"
+        Me.lblIc.AutoSize = True
+        Me.lblIc.Location = New System.Drawing.Point(725, 388)
+        Me.lblIc.Name = "lblIc"
+        Me.lblIc.Size = New System.Drawing.Size(105, 22)
+        Me.lblIc.TabIndex = 11
+        Me.lblIc.Text = "IC Number :"
         '
-        'Label5
+        'lblSubject
         '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(725, 450)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(80, 22)
-        Me.Label5.TabIndex = 12
-        Me.Label5.Text = "Subject :"
+        Me.lblSubject.AutoSize = True
+        Me.lblSubject.Location = New System.Drawing.Point(725, 450)
+        Me.lblSubject.Name = "lblSubject"
+        Me.lblSubject.Size = New System.Drawing.Size(80, 22)
+        Me.lblSubject.TabIndex = 12
+        Me.lblSubject.Text = "Subject :"
         '
         'Label6
         '
@@ -145,33 +145,33 @@ Partial Class TeacherInfo
         Me.Label6.TabIndex = 13
         Me.Label6.Text = "Add / Delete Teacher "
         '
-        'Button2
+        'btnAdd
         '
-        Me.Button2.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.Button2.Location = New System.Drawing.Point(729, 505)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(91, 31)
-        Me.Button2.TabIndex = 14
-        Me.Button2.Text = "Add"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btnAdd.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btnAdd.Location = New System.Drawing.Point(729, 505)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(91, 31)
+        Me.btnAdd.TabIndex = 14
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
         '
-        'Button3
+        'btnDelete
         '
-        Me.Button3.Location = New System.Drawing.Point(1000, 505)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(91, 31)
-        Me.Button3.TabIndex = 15
-        Me.Button3.Text = "Delete"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.btnDelete.Location = New System.Drawing.Point(1000, 505)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(91, 31)
+        Me.btnDelete.TabIndex = 15
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
         '
-        'Button4
+        'btnUpdate
         '
-        Me.Button4.Location = New System.Drawing.Point(867, 505)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(95, 31)
-        Me.Button4.TabIndex = 16
-        Me.Button4.Text = "Update"
-        Me.Button4.UseVisualStyleBackColor = True
+        Me.btnUpdate.Location = New System.Drawing.Point(867, 505)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(95, 31)
+        Me.btnUpdate.TabIndex = 16
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
         '
         'Label7
         '
@@ -189,20 +189,20 @@ Partial Class TeacherInfo
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1121, 707)
         Me.Controls.Add(Me.Label7)
-        Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnAdd)
         Me.Controls.Add(Me.Label6)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblSubject)
+        Me.Controls.Add(Me.lblIc)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.lblIcNumber)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.TextBox4)
-        Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtIcNumber)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.txtSubject)
+        Me.Controls.Add(Me.txtBoxIcNumber)
         Me.Controls.Add(Me.DataGridView1)
         Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Name = "TeacherInfo"
@@ -213,19 +213,19 @@ Partial Class TeacherInfo
 
     End Sub
     Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox3 As TextBox
-    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents txtBoxIcNumber As TextBox
+    Friend WithEvents txtSubject As TextBox
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtIcNumber As TextBox
     Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label4 As Label
-    Friend WithEvents Label5 As Label
+    Friend WithEvents lblIcNumber As Label
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents lblName As Label
+    Friend WithEvents lblIc As Label
+    Friend WithEvents lblSubject As Label
     Friend WithEvents Label6 As Label
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button4 As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnUpdate As Button
     Friend WithEvents Label7 As Label
 End Class
--- FYP/FYP/TeacherInfo.vb
+++ FYP/FYP/TeacherInfo.vb
@@ -1,3 +1,115 @@
-﻿Public Class TeacherInfo
+﻿Imports MySql.Data.MySqlClient
 
+Public Class TeacherInfo
+    Dim connectionString As String = "server=localhost; user=root; password=; database=school_management"
+    Dim connection As MySqlConnection = New MySqlConnection(connectionString)
+
+    Private Sub TeacherInfo_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadTeacherList()
+    End Sub
+    Private Sub LoadTeacherList()
+        Try
+            connection.Open()
+            Dim query As String = "SELECT * FROM teachers"
+            Dim adapter As New MySqlDataAdapter(query, connection)
+            Dim table As New DataTable()
+            adapter.Fill(table)
+            DataGridView1.DataSource = table
+        Catch ex As Exception
+            MessageBox.Show("Error loading teacher list: " & ex.Message)
+        Finally
+            connection.Close()
+        End Try
+    End Sub
+
+    ' Search for a teacher by IC number
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        Try
+            connection.Open()
+            Dim query As String = "SELECT Name, IC, Subject FROM teachers WHERE IC = @IC"
+            Dim command As New MySqlCommand(query, connection)
+            command.Parameters.AddWithValue("@IC", txtIcNumber.Text)
+
+            Dim reader As MySqlDataReader = command.ExecuteReader()
+            If reader.Read() Then
+                txtName.Text = reader("Name").ToString()
+                txtSubject.Text = reader("Subject").ToString()
+                MessageBox.Show("Teacher found!")
+            Else
+                MessageBox.Show("Teacher not found.")
+            End If
+            reader.Close()
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            connection.Close()
+        End Try
+    End Sub
+
+    ' Add a new teacher to the database
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Try
+            connection.Open()
+            Dim query As String = "INSERT INTO teachers (Name, IC, Subject) VALUES (@Name, @IC, @Subject)"
+            Dim command As New MySqlCommand(query, connection)
+            command.Parameters.AddWithValue("@Name", txtName.Text)
+            command.Parameters.AddWithValue("@IC", txtIcNumber.Text)
+            command.Parameters.AddWithValue("@Subject", txtSubject.Text)
+
+            If command.ExecuteNonQuery() > 0 Then
+                MessageBox.Show("Teacher added successfully.")
+                LoadTeacherList() ' Refresh the DataGridView
+            Else
+                MessageBox.Show("Failed to add teacher.")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            connection.Close()
+        End Try
+    End Sub
+
+    ' Update an existing teacher's details
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Try
+            connection.Open()
+            Dim query As String = "UPDATE teachers SET Name = @Name, Subject = @Subject WHERE IC = @IC"
+            Dim command As New MySqlCommand(query, connection)
+            command.Parameters.AddWithValue("@Name", txtName.Text)
+            command.Parameters.AddWithValue("@Subject", txtSubject.Text)
+            command.Parameters.AddWithValue("@IC", txtIcNumber.Text)
+
+            If command.ExecuteNonQuery() > 0 Then
+                MessageBox.Show("Teacher updated successfully.")
+                LoadTeacherList() ' Refresh the DataGridView
+            Else
+                MessageBox.Show("Failed to update teacher.")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            connection.Close()
+        End Try
+    End Sub
+
+    ' Delete a teacher from the database
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Try
+            connection.Open()
+            Dim query As String = "DELETE FROM teachers WHERE IC = @IC"
+            Dim command As New MySqlCommand(query, connection)
+            command.Parameters.AddWithValue("@IC", txtIcNumber.Text)
+
+            If command.ExecuteNonQuery() > 0 Then
+                MessageBox.Show("Teacher deleted successfully.")
+                LoadTeacherList() ' Refresh the DataGridView
+            Else
+                MessageBox.Show("Failed to delete teacher.")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            connection.Close()
+        End Try
+    End Sub
 End Class
