bb8796a524faabbd1e2ef1821d2bab824dc6b374
ChickenGyoza <yusriizzet2801@gmail.com>
2024-11-01 11:17:24 +0800

    Subject Information
    1/11/2024

---
+++ FYP/SUBJECT_INFORMATION.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtSubjectCode = New System.Windows.Forms.TextBox()
+        Me.txtSubjectName = New System.Windows.Forms.TextBox()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtSubjectCode
+        '
+        Me.txtSubjectCode.Location = New System.Drawing.Point(227, 113)
+        Me.txtSubjectCode.Name = "txtSubjectCode"
+        Me.txtSubjectCode.Size = New System.Drawing.Size(130, 22)
+        Me.txtSubjectCode.TabIndex = 0
+        '
+        'txtSubjectName
+        '
+        Me.txtSubjectName.Location = New System.Drawing.Point(227, 170)
+        Me.txtSubjectName.Name = "txtSubjectName"
+        Me.txtSubjectName.Size = New System.Drawing.Size(130, 22)
+        Me.txtSubjectName.TabIndex = 1
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(227, 264)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 23)
+        Me.btnSave.TabIndex = 2
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(25, 119)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(156, 16)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Enter your subject code : "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(25, 176)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(156, 16)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Enter your subject name :"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.txtSubjectName)
+        Me.Controls.Add(Me.txtSubjectCode)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtSubjectCode As TextBox
+    Friend WithEvents txtSubjectName As TextBox
+    Friend WithEvents btnSave As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ FYP/SUBJECT_INFORMATION.vb
@@ -0,0 +1,48 @@
+﻿Imports System.Data.SqlClient
+Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    ' Define connection string (update with your own server, username, password if necessary)
+    Private connectionString As String = "Server=127.0.0.1;Database=school_management;Uid=root;Pwd=;"
+
+    ' Method to open a connection to the database
+    Private Function GetConnection() As MySqlConnection
+        Return New MySqlConnection(connectionString)
+    End Function
+
+    ' Method to save subject information to the database
+    Private Sub SaveSubject()
+        ' Validate inputs
+        If String.IsNullOrEmpty(txtSubjectCode.Text) OrElse String.IsNullOrEmpty(txtSubjectName.Text) Then
+            MessageBox.Show("Please fill in all fields.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        ' SQL Insert Command
+        Dim query As String = "INSERT INTO subjects (subject_code, subject_name) VALUES (@subject, @subject_registered)"
+
+
+        ' Connect to the database
+        Using connection As MySqlConnection = GetConnection()
+            Using command As New MySqlCommand(query, connection)
+                ' Define parameters and their values
+                command.Parameters.AddWithValue("@subjects", txtSubjectCode.Text)
+                command.Parameters.AddWithValue("@subject_registered", txtSubjectName.Text)
+
+                ' Open connection and execute command
+                Try
+                    connection.Open()
+                    command.ExecuteNonQuery()
+                    MessageBox.Show("Subject information saved successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Catch ex As MySqlException
+                    MessageBox.Show("Error saving data: " & ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End Try
+            End Using
+        End Using
+    End Sub
+
+    ' Event handler for the Save button
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        SaveSubject()
+    End Sub
+End Class
